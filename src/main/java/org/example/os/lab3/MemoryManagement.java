5dfdec641fe1a08c2d6b070da4141b6d9e151b13
Ihor Tarkhan <ihor.tarkhan.working@gmail.com>
2021-11-28 09:26:39 +0200

    refactored MemoryManagement

--- src/main/java/org/example/os/lab3/MemoryManagement.java
+++ src/main/java/org/example/os/lab3/MemoryManagement.java
@@ -1,61 +1,36 @@
-package org.example.os.lab3;// The main MemoryManagement program, created by Alexander Reeder, 2000 Nov 19
+package org.example.os.lab3;
 
-import java.applet.*;
-import java.awt.*;
-import java.io.*;
-import java.util.*;
+import java.io.File;
 
-public class MemoryManagement
-{
-  public static void main(String[] args)
-  {
-    ControlPanel controlPanel;
-    Kernel kernel;
+public class MemoryManagement {
+    public static void main(String[] args) {
+        if (args.length < 1 || args.length > 2) {
+            throw new RuntimeException("Usage: 'java MemoryManagement <COMMAND FILE> <PROPERTIES FILE>'");
+        }
 
-    if ( args.length < 1 || args.length > 2 )
-    {
-      System.out.println( "Usage: 'java MemoryManagement <COMMAND FILE> <PROPERTIES FILE>'" );
-      System.exit( -1 );
+        validateFile(args[0]);
+        String commandFile = args[0];
+
+        String propertiesFile;
+        if (args.length == 2) {
+            validateFile(args[1]);
+            propertiesFile = args[1];
+        } else {
+            propertiesFile = null;
+        }
+
+        ControlPanel controlPanel = new ControlPanel("Memory Management");
+        Kernel kernel = new Kernel();
+        controlPanel.init(kernel, commandFile, propertiesFile);
     }
 
-    File f = new File( args[0] );
-
-    if ( ! ( f.exists() ) )
-    {
-      System.out.println( "MemoryM: error, file '" + f.getName() + "' does not exist." );
-      System.exit( -1 );
+    private static void validateFile(String fileSrc) {
+        File file = new File(fileSrc);
+        if (!(file.exists())) {
+            throw new RuntimeException("MemoryM: error, file '" + file.getName() + "' does not exist.");
+        }
+        if (!(file.canRead())) {
+            throw new RuntimeException("MemoryM: error, read of " + file.getName() + " failed.");
+        }
     }
-    if ( ! ( f.canRead() ) )
-    {
-      System.out.println( "MemoryM: error, read of " + f.getName() + " failed." );
-      System.exit( -1 );
-    }
-
-    if ( args.length == 2 )
-    {
-      f = new File( args[1] );
-
-      if ( ! ( f.exists() ) )
-      {
-        System.out.println( "MemoryM: error, file '" + f.getName() + "' does not exist." );
-        System.exit( -1 );
-      }
-      if ( ! ( f.canRead() ) )
-      {
-        System.out.println( "MemoryM: error, read of " + f.getName() + " failed." );
-        System.exit( -1 );
-      }
-    }
-
-    kernel = new Kernel();
-    controlPanel = new ControlPanel( "Memory Management" );
-    if ( args.length == 1 )
-    {
-      controlPanel.init( kernel , args[0] , null );
-    }
-    else
-    {
-      controlPanel.init( kernel , args[0] , args[1] );
-    }
-  }
 }
