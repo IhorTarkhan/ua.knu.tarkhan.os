a8f5c07696f005d423f5baaef54c893285611658
Ihor Tarkhan <ihor.tarkhan.working@gmail.com>
2021-11-29 22:08:11 +0200

    code format

--- src/main/java/org/example/os/lab3/ControlPanel.java
+++ src/main/java/org/example/os/lab3/ControlPanel.java
@@ -7,275 +7,262 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-public class ControlPanel extends Frame
-{
-  Kernel kernel ;
-  Button runButton = new Button("run");
-  Button stepButton = new Button("step");
-  Button resetButton = new Button("reset");
-  Button exitButton = new Button("exit");
-  List<Button> buttons;
-  List<Label> labels;
-  Label statusValueLabel = new Label("STOP" , Label.LEFT) ;
-  Label timeValueLabel = new Label("0" , Label.LEFT) ;
-  Label instructionValueLabel = new Label("NONE" , Label.LEFT) ;
-  Label addressValueLabel = new Label("NULL" , Label.LEFT) ;
-  Label pageFaultValueLabel = new Label("NO" , Label.LEFT) ;
-  Label virtualPageValueLabel = new Label("x" , Label.LEFT) ;
-  Label physicalPageValueLabel = new Label("0" , Label.LEFT) ;
-  Label RValueLabel = new Label("0" , Label.LEFT) ;
-  Label MValueLabel = new Label("0" , Label.LEFT) ;
-  Label inMemTimeValueLabel = new Label("0" , Label.LEFT) ;
-  Label lastTouchTimeValueLabel = new Label("0" , Label.LEFT) ;
-  Label lowValueLabel = new Label("0" , Label.LEFT) ;
-  Label highValueLabel = new Label("0" , Label.LEFT) ;
-  private String commands;
-  private String config;
+public class ControlPanel extends Frame {
+    Kernel kernel;
+    Button runButton = new Button("run");
+    Button stepButton = new Button("step");
+    Button resetButton = new Button("reset");
+    Button exitButton = new Button("exit");
+    List<Button> buttons;
+    List<Label> labels;
+    Label statusValueLabel = new Label("STOP", Label.LEFT);
+    Label timeValueLabel = new Label("0", Label.LEFT);
+    Label instructionValueLabel = new Label("NONE", Label.LEFT);
+    Label addressValueLabel = new Label("NULL", Label.LEFT);
+    Label pageFaultValueLabel = new Label("NO", Label.LEFT);
+    Label virtualPageValueLabel = new Label("x", Label.LEFT);
+    Label physicalPageValueLabel = new Label("0", Label.LEFT);
+    Label RValueLabel = new Label("0", Label.LEFT);
+    Label MValueLabel = new Label("0", Label.LEFT);
+    Label inMemTimeValueLabel = new Label("0", Label.LEFT);
+    Label lastTouchTimeValueLabel = new Label("0", Label.LEFT);
+    Label lowValueLabel = new Label("0", Label.LEFT);
+    Label highValueLabel = new Label("0", Label.LEFT);
+    private String commands;
+    private String config;
 
-  public ControlPanel( String title )
-  {
-    super(title);
-  }
-
-  public void init(String commands, String config)
-  {
-    this.commands = commands;
-    this.config = config;
-    kernel = new Kernel() ;
-    kernel.setControlPanel( this );
-    setLayout( null );
-    setBackground( Color.white );
-    setForeground( Color.black );
-    setSize( 635 , 545 );
-    setFont( new Font( "Courier", Font.PLAIN, 12 ) );
-
-    runButton.setForeground( Color.blue );
-    runButton.setBackground( Color.lightGray );
-    runButton.setBounds( 0,25,70,15 );
-    add( runButton );
-
-    stepButton.setForeground( Color.blue );
-    stepButton.setBackground( Color.lightGray );
-    stepButton.setBounds( 70,25,70,15 );
-    add( stepButton );
-
-    resetButton.setForeground( Color.blue );
-    resetButton.setBackground( Color.lightGray );
-    resetButton.setBounds( 140,25,70,15 );
-    add( resetButton );
-
-    exitButton.setForeground( Color.blue );
-    exitButton.setBackground( Color.lightGray );
-    exitButton.setBounds( 210,25,70,15 );
-    add( exitButton );
-
-    buttons = IntStream.range(0, 64)
-            .boxed()
-            .map(i -> {
-              Button button = new Button("page " + i);
-              button.setBounds(i / 32 == 0 ? 0 : 140, (i % 32 + 2) * 15 + 25, 70, 15);
-              button.setForeground(Color.magenta);
-              button.setBackground(Color.lightGray);
-              add(button);
-              return button;
-            })
-            .collect(Collectors.toList());
-
-    statusValueLabel.setBounds( 345, 25,100,15 );
-    add( statusValueLabel );
-
-    timeValueLabel.setBounds( 345,15+25,100,15 );
-    add( timeValueLabel );
-
-    instructionValueLabel.setBounds( 385,45+25,100,15 );
-    add( instructionValueLabel );
-
-    addressValueLabel.setBounds(385,60+25,230,15);
-    add( addressValueLabel );
-
-    pageFaultValueLabel.setBounds( 385,90+25,100,15 );
-    add( pageFaultValueLabel );
-
-    virtualPageValueLabel.setBounds( 395,120+25,200,15 );
-    add( virtualPageValueLabel );
-
-    physicalPageValueLabel.setBounds( 395,135+25,200,15 );
-    add( physicalPageValueLabel );
-
-    RValueLabel.setBounds( 395,150+25,200,15 );
-    add( RValueLabel );
-
-    MValueLabel.setBounds( 395,165+25,200,15 );
-    add( MValueLabel );
-
-    inMemTimeValueLabel.setBounds(395,180+25,200,15 );
-    add( inMemTimeValueLabel );
-
-    lastTouchTimeValueLabel.setBounds( 395,195+25,200,15 );
-    add( lastTouchTimeValueLabel );
-
-    lowValueLabel.setBounds( 395,210+25,230,15 );
-    add( lowValueLabel );
-
-    highValueLabel.setBounds( 395,225+25,230,15 );
-    add( highValueLabel );
-
-    Label virtualOneLabel = new Label( "virtual" , Label.CENTER) ;
-    virtualOneLabel.setBounds(0,15+25,70,15);
-    add(virtualOneLabel);
-
-    Label virtualTwoLabel = new Label( "virtual" , Label.CENTER) ;
-    virtualTwoLabel.setBounds(140,15+25,70,15);
-    add(virtualTwoLabel);
-
-    Label physicalOneLabel = new Label( "physical" , Label.CENTER) ;
-    physicalOneLabel.setBounds(70,15+25,70,15);
-    add(physicalOneLabel);
-
-    Label physicalTwoLabel = new Label( "physical" , Label.CENTER) ;
-    physicalTwoLabel.setBounds(210,15+25,70,15);
-    add(physicalTwoLabel);
-
-    Label statusLabel = new Label("status: " , Label.LEFT) ;
-    statusLabel.setBounds(285, 25,65,15);
-    add(statusLabel);
-
-    Label timeLabel = new Label("time: " , Label.LEFT) ;
-    timeLabel.setBounds(285,15+25,50,15);
-    add(timeLabel);
-
-    Label instructionLabel = new Label("instruction: " , Label.LEFT) ;
-    instructionLabel.setBounds(285,45+25,100,15);
-    add(instructionLabel);
-
-    Label addressLabel = new Label("address: " , Label.LEFT) ;
-    addressLabel.setBounds(285,60+25,85,15);
-    add(addressLabel);
-
-    Label pageFaultLabel = new Label("page fault: " , Label.LEFT) ;
-    pageFaultLabel.setBounds(285,90+25,100,15);
-    add(pageFaultLabel);
-
-    Label virtualPageLabel = new Label("virtual page: " , Label.LEFT) ;
-    virtualPageLabel.setBounds(285,120+25,110,15);
-    add(virtualPageLabel);
-
-    Label physicalPageLabel = new Label("physical page: " , Label.LEFT) ;
-    physicalPageLabel.setBounds(285,135+25,110,15);
-    add(physicalPageLabel);
-
-    Label RLabel = new Label("R: ", Label.LEFT) ;
-    RLabel.setBounds(285,150+25,110,15);
-    add(RLabel);
-
-    Label MLabel = new Label("M: " , Label.LEFT) ;
-    MLabel.setBounds(285,165+25,110,15);
-    add(MLabel);
-
-    Label inMemTimeLabel = new Label("inMemTime: " , Label.LEFT) ;
-    inMemTimeLabel.setBounds(285,180+25,110,15);
-    add(inMemTimeLabel);
-
-    Label lastTouchTimeLabel = new Label("lastTouchTime: " , Label.LEFT) ;
-    lastTouchTimeLabel.setBounds(285,195+25,110,15);
-    add(lastTouchTimeLabel);
-
-    Label lowLabel = new Label("low: " , Label.LEFT) ;
-    lowLabel.setBounds(285,210+25,110,15);
-    add(lowLabel);
-
-    Label highLabel = new Label("high: " , Label.LEFT) ;
-    highLabel.setBounds(285,225+25,110,15);
-    add(highLabel);
-
-    labels = IntStream.range(0, 64)
-            .boxed()
-            .map(i -> {
-              Label label = new Label(null, Label.CENTER);
-              label.setBounds(i / 32 == 0 ? 70 : 210, (i % 32 + 2) * 15 + 25, 60, 15);
-              label.setForeground(Color.red);
-              label.setFont(new Font("Courier", Font.PLAIN, 10));
-              add(label);
-              return label;
-            })
-            .collect(Collectors.toList());
-
-    kernel.init( commands , config );
-
-    setVisible(true);
-  }
-
-  public void paintPage( Page page )
-  {
-    virtualPageValueLabel.setText( Integer.toString( page.id ) );
-    physicalPageValueLabel.setText( Integer.toString( page.physical ) );
-    RValueLabel.setText( Integer.toString( page.R ) );
-    MValueLabel.setText( Integer.toString( page.M ) );
-    inMemTimeValueLabel.setText( Integer.toString( page.inMemTime ) );
-    lastTouchTimeValueLabel.setText( Integer.toString( page.lastTouchTime ) );
-    lowValueLabel.setText(Long.toString( page.low , Kernel.addressradix ) );
-    highValueLabel.setText(Long.toString( page.high , Kernel.addressradix ) );
-  }
-
-  public void setStatus(String status) {
-    statusValueLabel.setText(status);
-  }
-
-  public void addPhysicalPage( int pageNum , int physicalPage )
-  {
-    labels.get(physicalPage).setText("page " + pageNum);
-  }
-
-  public void removePhysicalPage( int physicalPage )
-  {
-    labels.get(physicalPage).setText(null);
-  }
-
-
-  public boolean action( Event e, Object arg )
-  {
-    Button target = (Button) e.target;
-    if ( target == runButton )
-    {
-      setStatus( "RUN" );
-      runButton.setEnabled(false);
-      stepButton.setEnabled(false);
-      resetButton.setEnabled(false);
-      kernel.start();
-      setStatus( "STOP" );
-      resetButton.setEnabled(true);
-      return true;
-    }
-    else if ( target == stepButton )
-    {
-      setStatus( "STEP" );
-      kernel.step();
-      if (kernel.runcycles == kernel.runs) {
-         stepButton.setEnabled(false);
-         runButton.setEnabled(false);
-      }
-      setStatus("STOP");
-      return true;
-    }
-    else if ( target == resetButton )
-    {
-      kernel = new Kernel() ;
-      kernel.setControlPanel( this );
-      kernel.init( commands , config );
-      runButton.setEnabled(true);
-      stepButton.setEnabled(true);
-      return true;
-    }
-    else if ( target == exitButton )
-    {
-      System.exit(0);
-      return true;
+    public ControlPanel(String title) {
+        super(title);
     }
 
-    if (buttons.contains(target)) {
-      kernel.getPage(buttons.indexOf(target));
-      return true;
+    public void init(String commands, String config) {
+        this.commands = commands;
+        this.config = config;
+        kernel = new Kernel();
+        kernel.setControlPanel(this);
+        setLayout(null);
+        setBackground(Color.white);
+        setForeground(Color.black);
+        setSize(635, 545);
+        setFont(new Font("Courier", Font.PLAIN, 12));
+
+        runButton.setForeground(Color.blue);
+        runButton.setBackground(Color.lightGray);
+        runButton.setBounds(0, 25, 70, 15);
+        add(runButton);
+
+        stepButton.setForeground(Color.blue);
+        stepButton.setBackground(Color.lightGray);
+        stepButton.setBounds(70, 25, 70, 15);
+        add(stepButton);
+
+        resetButton.setForeground(Color.blue);
+        resetButton.setBackground(Color.lightGray);
+        resetButton.setBounds(140, 25, 70, 15);
+        add(resetButton);
+
+        exitButton.setForeground(Color.blue);
+        exitButton.setBackground(Color.lightGray);
+        exitButton.setBounds(210, 25, 70, 15);
+        add(exitButton);
+
+        buttons = IntStream.range(0, 64)
+                .boxed()
+                .map(i -> {
+                    Button button = new Button("page " + i);
+                    button.setBounds(i / 32 == 0 ? 0 : 140, (i % 32 + 2) * 15 + 25, 70, 15);
+                    button.setForeground(Color.magenta);
+                    button.setBackground(Color.lightGray);
+                    add(button);
+                    return button;
+                })
+                .collect(Collectors.toList());
+
+        statusValueLabel.setBounds(345, 25, 100, 15);
+        add(statusValueLabel);
+
+        timeValueLabel.setBounds(345, 15 + 25, 100, 15);
+        add(timeValueLabel);
+
+        instructionValueLabel.setBounds(385, 45 + 25, 100, 15);
+        add(instructionValueLabel);
+
+        addressValueLabel.setBounds(385, 60 + 25, 230, 15);
+        add(addressValueLabel);
+
+        pageFaultValueLabel.setBounds(385, 90 + 25, 100, 15);
+        add(pageFaultValueLabel);
+
+        virtualPageValueLabel.setBounds(395, 120 + 25, 200, 15);
+        add(virtualPageValueLabel);
+
+        physicalPageValueLabel.setBounds(395, 135 + 25, 200, 15);
+        add(physicalPageValueLabel);
+
+        RValueLabel.setBounds(395, 150 + 25, 200, 15);
+        add(RValueLabel);
+
+        MValueLabel.setBounds(395, 165 + 25, 200, 15);
+        add(MValueLabel);
+
+        inMemTimeValueLabel.setBounds(395, 180 + 25, 200, 15);
+        add(inMemTimeValueLabel);
+
+        lastTouchTimeValueLabel.setBounds(395, 195 + 25, 200, 15);
+        add(lastTouchTimeValueLabel);
+
+        lowValueLabel.setBounds(395, 210 + 25, 230, 15);
+        add(lowValueLabel);
+
+        highValueLabel.setBounds(395, 225 + 25, 230, 15);
+        add(highValueLabel);
+
+        Label virtualOneLabel = new Label("virtual", Label.CENTER);
+        virtualOneLabel.setBounds(0, 15 + 25, 70, 15);
+        add(virtualOneLabel);
+
+        Label virtualTwoLabel = new Label("virtual", Label.CENTER);
+        virtualTwoLabel.setBounds(140, 15 + 25, 70, 15);
+        add(virtualTwoLabel);
+
+        Label physicalOneLabel = new Label("physical", Label.CENTER);
+        physicalOneLabel.setBounds(70, 15 + 25, 70, 15);
+        add(physicalOneLabel);
+
+        Label physicalTwoLabel = new Label("physical", Label.CENTER);
+        physicalTwoLabel.setBounds(210, 15 + 25, 70, 15);
+        add(physicalTwoLabel);
+
+        Label statusLabel = new Label("status: ", Label.LEFT);
+        statusLabel.setBounds(285, 25, 65, 15);
+        add(statusLabel);
+
+        Label timeLabel = new Label("time: ", Label.LEFT);
+        timeLabel.setBounds(285, 15 + 25, 50, 15);
+        add(timeLabel);
+
+        Label instructionLabel = new Label("instruction: ", Label.LEFT);
+        instructionLabel.setBounds(285, 45 + 25, 100, 15);
+        add(instructionLabel);
+
+        Label addressLabel = new Label("address: ", Label.LEFT);
+        addressLabel.setBounds(285, 60 + 25, 85, 15);
+        add(addressLabel);
+
+        Label pageFaultLabel = new Label("page fault: ", Label.LEFT);
+        pageFaultLabel.setBounds(285, 90 + 25, 100, 15);
+        add(pageFaultLabel);
+
+        Label virtualPageLabel = new Label("virtual page: ", Label.LEFT);
+        virtualPageLabel.setBounds(285, 120 + 25, 110, 15);
+        add(virtualPageLabel);
+
+        Label physicalPageLabel = new Label("physical page: ", Label.LEFT);
+        physicalPageLabel.setBounds(285, 135 + 25, 110, 15);
+        add(physicalPageLabel);
+
+        Label RLabel = new Label("R: ", Label.LEFT);
+        RLabel.setBounds(285, 150 + 25, 110, 15);
+        add(RLabel);
+
+        Label MLabel = new Label("M: ", Label.LEFT);
+        MLabel.setBounds(285, 165 + 25, 110, 15);
+        add(MLabel);
+
+        Label inMemTimeLabel = new Label("inMemTime: ", Label.LEFT);
+        inMemTimeLabel.setBounds(285, 180 + 25, 110, 15);
+        add(inMemTimeLabel);
+
+        Label lastTouchTimeLabel = new Label("lastTouchTime: ", Label.LEFT);
+        lastTouchTimeLabel.setBounds(285, 195 + 25, 110, 15);
+        add(lastTouchTimeLabel);
+
+        Label lowLabel = new Label("low: ", Label.LEFT);
+        lowLabel.setBounds(285, 210 + 25, 110, 15);
+        add(lowLabel);
+
+        Label highLabel = new Label("high: ", Label.LEFT);
+        highLabel.setBounds(285, 225 + 25, 110, 15);
+        add(highLabel);
+
+        labels = IntStream.range(0, 64)
+                .boxed()
+                .map(i -> {
+                    Label label = new Label(null, Label.CENTER);
+                    label.setBounds(i / 32 == 0 ? 70 : 210, (i % 32 + 2) * 15 + 25, 60, 15);
+                    label.setForeground(Color.red);
+                    label.setFont(new Font("Courier", Font.PLAIN, 10));
+                    add(label);
+                    return label;
+                })
+                .collect(Collectors.toList());
+
+        kernel.init(commands, config);
+
+        setVisible(true);
     }
-    return false;
-  }
+
+    public void paintPage(Page page) {
+        virtualPageValueLabel.setText(Integer.toString(page.id));
+        physicalPageValueLabel.setText(Integer.toString(page.physical));
+        RValueLabel.setText(Integer.toString(page.R));
+        MValueLabel.setText(Integer.toString(page.M));
+        inMemTimeValueLabel.setText(Integer.toString(page.inMemTime));
+        lastTouchTimeValueLabel.setText(Integer.toString(page.lastTouchTime));
+        lowValueLabel.setText(Long.toString(page.low, Kernel.addressradix));
+        highValueLabel.setText(Long.toString(page.high, Kernel.addressradix));
+    }
+
+    public void setStatus(String status) {
+        statusValueLabel.setText(status);
+    }
+
+    public void addPhysicalPage(int pageNum, int physicalPage) {
+        labels.get(physicalPage).setText("page " + pageNum);
+    }
+
+    public void removePhysicalPage(int physicalPage) {
+        labels.get(physicalPage).setText(null);
+    }
+
+
+    public boolean action(Event e, Object arg) {
+        Button target = (Button) e.target;
+        if (target == runButton) {
+            setStatus("RUN");
+            runButton.setEnabled(false);
+            stepButton.setEnabled(false);
+            resetButton.setEnabled(false);
+            kernel.start();
+            setStatus("STOP");
+            resetButton.setEnabled(true);
+            return true;
+        } else if (target == stepButton) {
+            setStatus("STEP");
+            kernel.step();
+            if (kernel.runcycles == kernel.runs) {
+                stepButton.setEnabled(false);
+                runButton.setEnabled(false);
+            }
+            setStatus("STOP");
+            return true;
+        } else if (target == resetButton) {
+            kernel = new Kernel();
+            kernel.setControlPanel(this);
+            kernel.init(commands, config);
+            kernel.reset();
+            runButton.setEnabled(true);
+            stepButton.setEnabled(true);
+            return true;
+        } else if (target == exitButton) {
+            System.exit(0);
+            return true;
+        }
+
+        if (buttons.contains(target)) {
+            kernel.getPage(buttons.indexOf(target));
+            return true;
+        }
+        return false;
+    }
 }
